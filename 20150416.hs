60adf517318eb664a1931256738c8e37e5bd7ef5
LucasdeSouzaAlbuquerque <lsa2@cin.ufpe.br>
2015-04-16 07:05:59 -0300

    p

--- 20150416.hs
+++ 20150416.hs
@@ -1,13 +1,13 @@
 {-listPartitioner-}
-listPartitioner :: [Ord t] => [t] -> ([t]->[[t]])
+listPartitioner :: [Int] -> ([Int]->[[Int]])
 listPartitioner [] = tabajara []
 listPartitioner a = tabajara (quicksort (a))
 
-tabajara :: [Ord t] => [t] -> [t] -> [[t]]
+tabajara :: [Int] -> [Int] -> [[Int]]
 tabajara [] b = []
 tabajara (a:as) b | as /= [] = [[y | y <- quicksort(b), y <= a]] ++ tabajara as [y | y <- b, y > a]
                   | otherwise = [[y | y <- quicksort(b), y <= a]] ++ [[y | y <- b, y > a]]
 				 
-quicksort :: [Ord t] => [t] -> [t]
+quicksort :: [Int] -> [Int]
 quicksort [] = []
 quicksort (pivot:rest) = quicksort[y | y <- rest, y <= pivot] ++ [pivot] ++ quicksort[y | y <- rest, y > pivot]
